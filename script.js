bd6d749310c815060c2d043a1a58d6378198c2df
karolinakv <karolinavalentova2@gmail.com>
2019-09-04 17:44:20 +0200

    added Sort by first name in JS

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ async function processStudentData() {
         const jsonStudentData = await (await fetch("./data/students.json")).json();
 
         studentData = jsonStudentData ? jsonStudentData : [];
+        showStudentData(studentData);
+
     } catch(error) {
         studentData = [];
         console.error('Cannot read student list, reason: ' + error.message);
     }
 }
 
-function showStudentData() {
+function showStudentData(studentsArray) {
     const studentListElement = document.getElementById('studentList');
     const studentListEntryTemplate = document.getElementById('studentEntry');
 
-    studentData.forEach((studentEntry) => {
+    studentsArray.forEach((studentEntry) => {
         let temporaryStudentEntryTemplate = studentListEntryTemplate.content.cloneNode(true);
 
         temporaryStudentEntryTemplate.childNodes[1].childNodes[1].textContent = studentEntry['fullname'];
@@ -27,3 +29,19 @@ function showStudentData() {
     })
 }
 
+
+
+// Sort by FIRST NAME
+
+
+
+function sortByFirstName() {
+    let sortedArray = studentData.sort((a,b) => {
+        if (a['fullname'] < b['fullname']) return -1;
+        if (a['fullname'] > b['fullname']) return 1;
+        return 0;
+    });
+
+    deleteChilds(document.getElementById('studentList'));
+    showStudentData(sortedArray);
+}
